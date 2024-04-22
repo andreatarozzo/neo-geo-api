6a50c6e2bea3a69ed3568fa2d0d243d51726e4f7
Andrea Tarozzo <tarozzo.andrea@gmail.com>
2024-04-23 00:43:26 +0100

    chore: fixing comments

--- startup.sh
+++ startup.sh
@@ -4,9 +4,9 @@ echo "Waiting for DB container with HOST:${DB_HOST} PORT:${DB_PORT} to accept co
 
 # This command checks if the target database host is ready to accept connections.
 # It waits for 1 second before retrying if the connection is not yet available.
-# This is particularly useful in Docker Compose setups, where the database container
-# might pass health checks but still be initializing, preventing it from accepting connections
-# and so preventing the node application to fail its DB connection check.
+# This is particularly useful when running this kind of services with docker-compose because the database container
+# might pass health checks but still be in its initializing state, preventing it from accepting connections
+# and so making the node application fail its DB connection check.
 max_retries=60
 retries=0
 while ! nc -z ${DB_HOST} ${DB_PORT}; do
@@ -29,7 +29,7 @@ else
   # In test and development env we would like also to try to create the DB if not exists
   # and seed the db if it is not already filled with something ( logic in the seeder script )
   # This because we could be running the docker-compose command for the first time
-  # meaning that no volume was previously present for the DB.
+  # meaning that no volume was previously present for the DB and so the postgres instance would be empty.
   npx sequelize-cli db:create
   npx sequelize-cli db:migrate
   npx sequelize-cli db:seed:all
