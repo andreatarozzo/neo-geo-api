6fe8bacff388de014aee23a1b00f6635f4d3de02
Andrea Tarozzo <tarozzo.andrea@gmail.com>
2024-04-22 22:53:02 +0100

    feat: added startup logic and completed docker-compose

--- startup.sh
+++ startup.sh
@@ -1,1 +1,38 @@
+#!/bin/sh
+
+echo "Waiting for DB container with HOST:${DB_HOST} PORT:${DB_PORT} to accept connections..."
+
+# This command checks if the target database host is ready to accept connections.
+# It waits for 1 second before retrying if the connection is not yet available.
+# This is particularly useful in Docker Compose setups, where the database container
+# might pass health checks but still be initializing, preventing it from accepting connections
+# and so preventing the node application to fail its DB connection check.
+max_retries=60
+retries=0
+while ! nc -z ${DB_HOST} ${DB_PORT}; do
+ retries=$((retries+1))
+ if [ $retries -ge $max_retries ]; then
+    echo "Failed to connect to the database after $max_retries attempts."
+    exit 1
+ fi
+ echo "Connection attempt $retries failed. Retrying in 1 second..."
+ sleep 1
+done
+
+echo "DB container is ready!"
+
+if [ "$NODE_ENV" = "production" ]; then
+  # In production we assume that the DB is already created and 
+  # we would eventually just trigger a migration if any ( handled by sequelize )
+  npx sequelize-cli db:migrate
+else
+  # In test and development env we would like also to try to create the DB if not exists
+  # and seed the db if it is not already filled with something ( logic in the seeder script )
+  # This because we could be running the docker-compose command for the first time
+  # meaning that no volume was previously present for the DB.
+  npx sequelize-cli db:create
+  npx sequelize-cli db:migrate
+  npx sequelize-cli db:seed:all
+fi
+
 yarn start
